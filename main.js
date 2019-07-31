95bee0fe70ccbe64b96ad0fe6b39e9f6c535e10e
Emil Litwiniec <emilitwiniec@gmail.com>
2019-07-31 22:14:41 +0200

    Add initial js logic / Projects carousel section.

---
+++ main.js
@@ -0,0 +1,35 @@
+const btnPrev = document.getElementById('btn-prev');
+const btnNext = document.getElementById('btn-next');
+
+let projectIndex = 1;
+function setIndex(action) {
+  const previousProjectIndex = projectIndex;
+  if(action === "prev") {
+    if(projectIndex - 1 < 1) {
+      projectIndex = 8
+    } else {
+      projectIndex-- 
+    }
+  } else if(action === "next") {
+    if(projectIndex + 1 > 8) {
+      projectIndex = 1
+    } else {
+      projectIndex++
+    }
+  }
+  console.log('previous index', previousProjectIndex)
+  console.log('actual index',projectIndex);
+  // setActive(projectIndex);
+}
+const handleActive = {
+  set(index) {
+
+  },
+  remove(index) {
+
+  }
+
+}
+
+btnPrev.addEventListener('click', () => setIndex('prev'));
+btnNext.addEventListener('click', () => setIndex('next'));
